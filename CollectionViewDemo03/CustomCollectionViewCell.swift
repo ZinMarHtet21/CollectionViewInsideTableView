4eda29b5ef65664c6c133d03ff8ebb283243e86e
Zin Mar Htet <zinmarhtet@Zins-MacBook-Pro.local>
2017-09-23 20:11:11 +0800

    first commit

---
+++ CollectionViewDemo03/CustomCollectionViewCell.swift
@@ -0,0 +1,13 @@
+//
+//  CustomCollectionViewCell.swift
+//  CollectionViewDemo03
+//
+//  Created by Zin Mar Htet on 22/9/17.
+//  Copyright © 2017 Zin Mar Htet. All rights reserved.
+//
+
+import UIKit
+
+class CustomCollectionViewCell: UICollectionViewCell {
+    
+}
---
+++ CollectionViewDemo03/CustomTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  CustomTableViewCell.swift
+//  CollectionViewDemo03
+//
+//  Created by Zin Mar Htet on 22/9/17.
+//  Copyright © 2017 Zin Mar Htet. All rights reserved.
+//
+
+import UIKit
+
+class CustomTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
